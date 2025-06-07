020024cc9c00f4132722dfd2be0f7abf08fe18fb
Sergey Tushev <sergy5@mail.ru>
2025-06-08 01:39:40 +0300

    initial commit

---
+++ env.sh
@@ -0,0 +1,6 @@
+pip install poetry  # ставим poetry как системный пакет (или через venv)
+
+# Windows: добавить путь до poetry в PATH
+
+poetry --version  # проверяем версию
+poetry install  # устанавливаем виртуальное окружение (автоматически)
